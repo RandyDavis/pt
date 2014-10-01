f164624978c5dabeb6cb6a10becacb85b3475731
Randy Davis <randy_davis@outlook.com>
2014-10-01 12:29:29 -0500

    Configure devise and install devise views.  Created partials in layouts directory.

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :plans
+
+  ROLES = [:admin, :trainer, :client]
+
+  # def role_symbols
+  #   [role.to_sym]
+  # end
+
+  def role?(role_name)
+    # TODO
+  end
+end
